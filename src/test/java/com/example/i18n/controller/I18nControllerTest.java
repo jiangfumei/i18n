60537bf2b8fd5159c3590092351efbdfcdeee3f3
jiangfumei <40990879+jiangfumei@users.noreply.github.com>
2020-12-02 15:53:50 +0800

    test

--- src/test/java/com/example/i18n/controller/I18nControllerTest.java
+++ src/test/java/com/example/i18n/controller/I18nControllerTest.java
@@ -1,11 +1,14 @@
 package com.example.i18n.controller;
 
 import com.example.i18n.BasicMvc;
+import org.apache.tomcat.jni.Local;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Locale;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,40 +21,44 @@ public class I18nControllerTest extends BasicMvc {
 
     @Test
     public void message() throws Exception {
-        mockMvc.perform(//
-                get("/getMessage")//
-                        //.header("Accept-Language", "zh_CN")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON_VALUE) )//
-                .andDo(print())//
-                .andExpect(status().isOk())//
-                .andExpect(content().string("中文"))
-
-        ;
-        mockMvc.perform(//
+        String china = mockMvc.perform(//
                 get("/getMessage")//
                         .header("Accept-Language", "zh")
-                        .contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON_VALUE) )//
-                .andDo(print())//
-                .andExpect(status().isOk())//
-                .andExpect(content().string("chinese"))
+                        .locale(Locale.CHINA)
+                        )//
+        .andReturn()
+        .getResponse()
+        .getContentAsString();
+        System.out.println(china);
 
-        ;
-        mockMvc.perform(//
+        String en = mockMvc.perform(//
                 get("/getMessage")//
-                        .header("Accept-Language", "ja"))//
-                .andDo(print())//
-                .andExpect(status().isOk())//
-                .andExpect(content().string("あ"))
+                        .header("Accept-Language", "en")
+                        .locale(Locale.ENGLISH)
+        )//
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        System.out.println(en);
 
-        ;
-        mockMvc.perform(//
+        String ja = mockMvc.perform(//
                 get("/getMessage")//
-                        .header("Accept-Language", "en"))//
-                .andDo(print())//
-                .andExpect(status().isOk())//
-                .andExpect(content().string("english"))
+                        .header("Accept-Language", "ja")
+                        .locale(Locale.JAPAN)
+        )//
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        System.out.println(ja);
 
-        ;
+//        mockMvc.perform(//
+//                get("/getMessage")//
+//                        .header("Accept-Language", "en"))//
+//                .andDo(print())//
+//                .andExpect(status().isOk())//
+//                .andExpect(content().string("english"))
+//
+//        ;
 
     }
 }
